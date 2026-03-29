d9d146b25be3d9a7918aa49124b38618fcccb52a
bhlk64 <bhlk64.github.com>
2026-03-29 22:45:28 +0700

    build sdcard

---
+++ sdcard.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "ff.h"
+
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS   17
+#define PIN_SCK  18
+#define PIN_MOSI 19
+
+int main() {
+    stdio_init_all();
+
+    spi_init(SPI_PORT, 1000 * 1000);
+
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+
+    gpio_init(PIN_CS);
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "", 1);
+
+    if (fr != FR_OK) {
+        printf("Mount fail\n");
+        while (1);
+    }
+
+    FIL fil;
+    char buf[100];
+
+    fr = f_open(&fil, "test.txt", FA_READ);
+    if (fr == FR_OK) {
+        f_gets(buf, sizeof(buf), &fil);
+        printf("Read: %s\n", buf);
+        f_close(&fil);
+    } else {
+        printf("Open fail\n");
+    }
+
+    while (1);
+}

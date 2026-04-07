7c31f80b15325617a15b21e43327703b939c5782
bhlk64 <hoangthivang1974pl@gmail.com>
2026-04-07 19:52:34 +0700

    build

--- blink+read.c
+++ blink+read.c
@@ -1,27 +1,49 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ff.h"
+
+void fail(int f) {
+    while (f) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(200);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(200);
+        f--;
+    }
+    while(1);
+}
 
 int main() {
     const uint LED_PIN = 25;  // onboard LED
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     
-    FILE *f = fopen("times.txt","r");
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "", 1);
     
-    if (f==NULL) {
-        gpio_put(LED_PIN,1);
-        while(1);
+    //FILE *f = fopen("times.txt","r");
+    
+    if (fr!=FR_OK) {fail(1);}
+    
+    FIL f;
+    char buf[16];
+    int times;
+    if (f_open(&f,"times.txt",FA_READ)==FR_OK) {
+        if (f_gets(buf, sizeof(buf),&f)) {
+            times = atoi(buf);
+        }
     }
     
-    int times;
-    fscanf(f,"%d",&times);
-    fclose(f);
+    f_close(&f);
 
     while (times) {
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
         gpio_put(LED_PIN, 0);
         sleep_ms(500);
+        times--;
     }
     while(1);
 }
